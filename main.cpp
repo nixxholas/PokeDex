5bcc6b50372e353fc5ad337bfb22b12567576b08
Nicholas <amperific@gmail.com>
2016-10-20 21:46:36 +0800

    Source: JSON for Modern C++: Implementation
    Signed-off-by: Nicholas <amperific@gmail.com>

--- main.cpp
+++ main.cpp
@@ -5,12 +5,18 @@
 #include <iostream>
 #include <fstream>
 #include <filesystem>
+#include "json.hpp"
 
 // Directory Namespace Inclusion
 // https://msdn.microsoft.com/en-us/library/hh874694.aspx
 using namespace std::tr2::sys;
 
+// https://github.com/nlohmann/json
+using json = nlohmann::json;
+
 void initializePokemons() {
+	std::string line;
+	int i = 0;
 	// For debugging purposes only
 	//int i = 1;
 
@@ -33,22 +39,50 @@ void initializePokemons() {
 		// Finally just convert the stream to a string
 		std::string path = stream.str();
 
+		// Current jsonContent
+		json jsonContent;
+
 		// Reads the file from the string path
 		std::ifstream in(path);
 
+		// Parse the Stream to the Json Object
+		in >> jsonContent;
+
+		// Debugging jsonContent
+		std::cout << jsonContent["name"] << std::endl;
+
+
+		// ------------------------------- DEPRECATED CODE ----------------------------- //
+
+		// Iterate through each line in the ifstream
+		// http://stackoverflow.com/questions/13103296/reading-from-file-line-by-line-in-ceach-line-has-one-string-and-several-int-v
+		//while (std::getline(in, line)) {
+		//	if (i == 1) { // Index
+		//		std::cout << line << std::endl;
+		//	}
+		//	else if (i == 2) { // Name of Pokemon
+		//		std::cout << line << std::endl;
+		//	}
+		//	i++;
+		//}
+
 		// Read the contents of the file via the ifstream object
-		std::string contents((std::istreambuf_iterator<char>(in)),
-			std::istreambuf_iterator<char>());
-
+		//std::string contents((std::istreambuf_iterator<char>(in)),
+		//	std::istreambuf_iterator<char>());
+		
 		// Print out what is contained within the file.
-		std::cout << contents << std::endl;
+		//std::cout << contents << std::endl;
 
+		// --------------------------- END OF DEPRECATED CODE ------------------------- //
+
+		// Reset the integer
+		i = 0;
 	}
 
 	/*
 	Hard-Coded Code for File Directory Reading
 
-	*Deprecated*
+	* Deprecated *
 	
 	for (size_t i = 1; i <= 10; i++)
 	{
